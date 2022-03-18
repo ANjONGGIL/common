7b3228a948ad17d948d6548fdd1577a6308bb6e0
ANJONGGIL <qws458@naver.com>
2022-03-18 19:33:28 +0900

    그리디
    -2747

---
+++ src/greedy/BaekJoon2747.java
@@ -0,0 +1,24 @@
+package greedy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BaekJoon2747 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(1);
+
+        for (int i = 2; i <= N; i++) {
+            list.add(list.get(i - 2) + list.get(i - 1));
+        }
+
+        System.out.println(list.get(N));
+
+    }
+}
