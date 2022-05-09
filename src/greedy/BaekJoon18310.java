63e4ef4c1abc72d0d38bc3b5d908607a6ca101cb
ANJONGGIL <qws458@naver.com>
2022-05-10 07:51:31 +0900

    그리디
    -백준 18310

---
+++ src/greedy/BaekJoon18310.java
@@ -0,0 +1,23 @@
+package greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BaekJoon18310 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Collections.sort(arr);
+
+        System.out.println(arr.get((N-1)/2));
+    }
+}
